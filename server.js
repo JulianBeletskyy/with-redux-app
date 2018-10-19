016d641c171a46c9139c0aba700b66adc4e88c73
JulianBeletskyy <jb@div-art.com>
2018-10-19 18:11:08 +0300

    commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ require('laravel-echo-server').run({
     authEndpoint: '/broadcasting/auth',
     devMode: true,
     database: "redis",
+    protocol: 'https',
     databaseConfig: {
         redis: {
             host: 'localhost',
